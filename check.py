e18e71d74f4ba02c67d6f673c2ff8a04e02328f7
eja <redzasp2303@gmail.com>
2024-11-12 22:40:22 +0700

    fix: 429 status code when using concurrent

--- check.py
+++ check.py
@@ -1,6 +1,11 @@
 import requests
 import sys
 import logging
+import time
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] - %(message)s')
@@ -9,17 +14,34 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 DEPENDENCIES_URL = {
     'python': 'https://pypi.org/project/{package_name}/',
-    'js': 'https://www.npmjs.com/package/{package_name}'
+    'js': 'https://registry.npmjs.org/{package_name}/'
 }
 
 # Read URLs from standard input and strip whitespace
 urls = [line.strip() for line in sys.stdin if line.strip()]
 
+def get_random_user_agent():
+    # you can also import SoftwareEngine, HardwareType, SoftwareType, Popularity from random_user_agent.params
+    # you can also set number of user agents required by providing `limit` as parameter
+
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
+    # Get Random User Agent String.
+    user_agent = user_agent_rotator.get_random_user_agent()
+    
+    return user_agent
+
 def is_unclaimed(package_name: str, language: str) -> tuple[bool,int]:
     """Check if a package name is unclaimed on npm."""
+    ua = get_random_user_agent()
+    
     logger.debug(f"Checking if package {package_name} exists on npmjs...")
     dependencies_url = DEPENDENCIES_URL[language]
-    r = requests.head(dependencies_url.format(package_name=package_name), headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"})
+    r = requests.head(dependencies_url.format(package_name=package_name), headers={"User-Agent":ua})
+        
     logger.debug(f"status code of package {package_name} is {r.status_code}")
     return r.status_code != 200 and r.status_code != 302, r.status_code  # Returns True if status is not 200 (unclaimed)
 
@@ -65,6 +87,6 @@ def main():
             url, vulnerabilities = future.result()
             for dependency in vulnerabilities:
                 logger.info(f"[VULN] {url} [{dependency['package_name']}|{dependency['status_code']}|{dependency['language']}]")
-
+        
 if __name__ == "__main__":
     main()
